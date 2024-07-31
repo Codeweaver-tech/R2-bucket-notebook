1509a7ff24fd0833e0a609451ea0766d08e06cde
Myring Muse <168040162+MyceliumMuse@users.noreply.github.com>
2024-07-30 21:57:05 -0400

    Create config_template.py
    Signed-off-by: Myring Muse <168040162+MyceliumMuse@users.noreply.github.com>

---
+++ interface/config/config_template.py
@@ -0,0 +1,6 @@
+# config_template.py
+# Rename this file to config.py and fill in your credentials
+
+ACCESS_KEY = 'your_access_key'
+SECRET_KEY = 'your_secret_key'
+ENDPOINT_URL = 'https://<account_id>.r2.cloudflarestorage.com'
